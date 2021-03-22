04f52bf610c326d7feda3e67a1c334af2d2703ba
9cbef11163ae1108cbc9d2a3dd11a553a5954695 <9cbef11163ae1108cbc9d2a3dd11a553a5954695>
2021-03-22 13:42:55 +0100

    update run.sh + add readme.html

--- run.sh
+++ run.sh
@@ -7,11 +7,11 @@ dirname=`basename "$PWD"`;
 basename="_mongo_1"; # A MODIFIER SI PROBLEME, par exemple plusieurs containers mongo donc numéro > 1
 container="${dirname,,}${basename}"; # ,, pour lowercase
 
-if (docker exec -i $container sh -c 'mongo /import/check.js --authenticationDatabase admin -u root -p example | grep yes')
+if (sudo docker exec -i $container sh -c 'mongo /import/check.js --authenticationDatabase admin -u root -p example | grep yes')
 then 
    	echo "La base de données contient déjà les données";
 else
 	echo "La base de données va recevoir les données"
-   	docker exec -i $container sh -c 'mongoimport --db weather_db --jsonArray --collection weather --type json --file /import/data_pays_2000-2021.json --authenticationDatabase admin -u root -p example';
+   	sudo docker exec -i $container sh -c 'mongoimport --db weather_db --jsonArray --collection weather --type json --file /import/data_pays_2000-2021.json --authenticationDatabase admin -u root -p example';
    	echo "Si le nom du container n'est pas valide veuillez modifier le nom du container dans RUN.sh";
 fi
